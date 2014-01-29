4eb386b6d01622e7e112f5c7733fe6264092bd10
Gary Verhaegen <gary.verhaegen@gmail.com>
2014-01-29 20:53:15 +0100

    Simplify project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,5 @@
-(defproject clojure-koans "0.5.0-SNAPSHOT"
+(defproject clojure-koans "0.5.0-Eclipse"
   :description "The Clojure koans."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [koan-engine "0.2.0"]]
-  :dev-dependencies [[lein-koan "0.1.2"]]
-  :profiles {:dev {:dependencies [[lein-koan "0.1.2"]]}}
-  :repl-options {:init-ns user}
-  :plugins [[lein-koan "0.1.2"]
-            [lein2-eclipse "2.0.0"]]
-  :main koan-engine.runner/exec)
+  :plugins [[lein2-eclipse "2.0.0"]])
